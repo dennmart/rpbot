64cea9546a93ffef0d68e8bf121943cb8a08fbb9
Dennis Martinez <dennis@dennmart.com>
2011-08-27 19:20:29 -0700

    A bit of cleanup before open-sourcing

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.room(process.env.CAMPFIRE_ROOM, function (room) {
         var msg = message.body.trim();
         if (msg.match(/last commit/i)) {
           var branch = msg.replace("rpbot last commit", "").trim() || "develop";
-          rpGithub.getLastCommitInfo(room, branch);
+          githubInfo.getLastCommitInfo(room, branch);
         } else if (msg.match(/happy hour/i)) {
           happyHour.currentStatus(room);
         } else if (msg.match(/weather/i)) {
